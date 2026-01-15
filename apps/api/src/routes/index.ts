628654f7a48a6aa67a99b8b2bbb891047d394cc3
nicotordev <nicotordev@gmail.com>
2026-01-15 08:52:53 -0300

    chore: Remove the resume parsing feature and all related API routes, controllers, and services.

--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -8,7 +8,6 @@ import userApp from "./user.route.js";
 import adminApp from "./admin.route.js";
 import cartApp from "./cart.route.js";
 import collectionApp from "./collection.route.js";
-import resumeApp from "./resume.route.js";
 import { stripeWebhookRoute } from "../services/stripe.webhook.js";
 
 const appRouter = new OpenAPIHono()
@@ -18,7 +17,6 @@ const appRouter = new OpenAPIHono()
   .route("/blocks", blockApp)
   .route("/cart", cartApp)
   .route("/collections", collectionApp)
-  .route("/resume", resumeApp)
   .route("/tags", tagApp)
   .route("/categories", categoryApp)
   .route("/commerce", purchaseApp)
